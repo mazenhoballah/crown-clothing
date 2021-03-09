4f92f0f1449cf64105505c87b2a27db5cd9d7b74
mazenhoballah <mazen.hoballah.mh@gmail.com>
2021-03-09 05:26:41 +0200

    git config --global user.name "John Doe"

--- src/App.js
+++ src/App.js
@@ -1,26 +1,13 @@
-<<<<<<< HEAD
-import './App.css';
-import HomePage from './pages/homepage/homepage.component';
-
-function App() {
-  return (
-    <div className="App">
-      <HomePage/>
-    </div>
-  );
-=======
 import React from 'react';
 import './App.css';
 import HomePage from './homepage.component';
 
 function App() {
-    return(
+    return (
         <div>
-            <HomePage/>
+            <HomePage />
         </div>
-    )
-     
->>>>>>> 99fda4f (created project with creat-react-app and added files for homepage)
+    );
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -2,17 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-<<<<<<< HEAD
-
-=======
-  
->>>>>>> 99fda4f (created project with creat-react-app and added files for homepage)
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+    <React.StrictMode>
+        <App />
+    </React.StrictMode>,
+    document.getElementById('root')
 );
 
 // If you want to start measuring performance in your app, pass a function
